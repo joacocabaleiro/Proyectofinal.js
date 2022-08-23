18b42242eec2d4c5c5233150e49a779bec58abbc
joacocabaleiro <joacocabaleiro@gmail.com>
2022-08-23 11:44:14 -0300

    Carrito casi

--- js/simulador.js
+++ js/simulador.js
@@ -1,24 +1,149 @@
-const productos =[  {nombre: "Mate 1", precio: 1500},
-                    {nombre: "Mate 2", precio: 1500},
-                    {nombre: "Bolsa 1", precio:2000},
-                    {nombre: "Bolsa 2", precio:2000}, 
-                    {nombre: "Caja 1", precio:2500},
-                    {nombre: "Caja 2", precio: 2500}, 
-                    {nombre: "Cartas", precio: 800}, 
-                    {nombre: "Combo 1", precio:5600}, 
-                    {nombre: "Combo 2", precio:5600}, 
-                    {nombre: "Combo 3", precio:5600}, 
-                    {nombre: "Combo 4", precio:5600}, 
-                    {nombre: "Cuaderno", precio:1200}]
+let stockProductos =[{id:1, nombre: "Mate 1", tipo:"mate", cantidad: 1, descripcion:"Un mate unico.", precio: 1500, img: '/imagenes/Mate1.jpeg'},
+                    {id:2, nombre: "Mate 2", tipo:"mate", cantidad: 1, descripcion:"Un mate unico.", precio: 1500,  img: '/imagenes/materosaamarillo.jpeg'},
+                    {id:3, nombre: "Bolsa 1", tipo:"bolso", cantidad: 1, descripcion:"Para que lleves todas tus cositas", precio:3000,  img: '/imagenes/Bolsomujermaravilla.jpeg'},
+                    {id:4, nombre: "Bolsa 2", tipo:"bolso", cantidad: 1, descripcion:"Para que lleves todas tus cositas", precio: 3000,  img: '/imagenes/Bolsosomosmagia.jpeg'}, 
+                    {id:5, nombre: "Caja 1", tipo:"caja", cantidad: 1, descripcion:"Una cajita imperfecta", precio: 8500,  img: '/imagenes/Cajitaimperfecta.jpeg'},
+                    {id:6, nombre: "Caja 2", tipo:"caja", cantidad: 1, descripcion:"Otra cajita imperfecta", precio: 8500,  img: '/imagenes/Cajitaimperfecta2.jpeg'}, 
+                    {id:7, nombre: "Cartas", tipo:"cartas", cantidad: 1, descripcion:"Descubri lo que esconden", precio: 3000,  img: '/imagenes/cartasintervenituvida.jpeg'}, 
+                    {id:8, nombre: "Combo 1", tipo:"combo", cantidad: 1, descripcion:"Para que no te falte nada, todo el combo.", precio: 11000,  img: '/imagenes/combo1.jpeg'}, 
+                    {id:9, nombre: "Combo 2", tipo:"combo", cantidad: 1, descripcion:"Para que no te falte nada, todo el combo.", precio: 11000,  img: '/imagenes/combo2.jpeg'}, 
+                    {id:10, nombre: "Combo 3", tipo:"combo", cantidad: 1, descripcion:"Para que no te falte nada, todo el combo.", precio: 11000,  img: '/imagenes/combo3.jpeg'}, 
+                    {id:11, nombre: "Combo 4", tipo:"combo", cantidad: 1, descripcion:"Para que no te falte nada, todo el combo.", precio: 11000,  img: '/imagenes/combo4.jpeg'}, 
+                    {id:12, nombre: "Cuaderno", tipo:"cuaderno", cantidad: 1, descripcion:"Aca escribi tus ideitas.", precio: 1000,  img: '/imagenes/cuadernocorazon.jpeg'}]
+
+const contenedorProductos = document.getElementById('contenedor-productos')
+
+const contenedorCarrito = document.getElementById('carrito-contenedor')
+
+const contadorCarrito = document.getElementById('contadorCarrito')
+
+const precioTotal= document.getElementById('precioTotal')
 
 let carrito = []
-let eleccion = prompt("Quisiera comprar algun producto?")
 
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        actualizarCarrito()
+    }
+})
+
+/*stockProductos.forEach((producto)=> {
+    const div =document.createElement('div')
+    div.classList.add('FotosComprar')
+    div.innerHTML = `
+    <img src=${producto.img}alt="">
+    <h3>${producto.nombre}</h3>
+    <p>${producto.descripcion}</p>
+    <p class="precioProducto">Precio:$ ${producto.precio}</p>
+    <button id= "agregar ${producto.id}" class="boton-agregar">Agregar <i class fas-fa-shopping-cart"></button>
+    `
+    contenedorProductos.appendChild(div)
+
+    const boton = document.getElementById('agregar ${producto.id}')
+
+    boton.addEventListener ('click', () => {
+        agregarAlCarrito(producto.id)
+    })
+
+})
+
+
+
+*/
+
+stockProductos.forEach((producto) => {
+    const div = document.createElement('div')
+    div.classList.add('producto')
+    div.innerHTML = `
+    <img src=${producto.img} alt= "">
+    <h3>${producto.nombre}</h3>
+    <p class="precioProducto">Precio:$ ${producto.precio}</p>
+    <button id="agregar${producto.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
+    `
+    contenedorProductos.appendChild(div)
+
+
+    const boton = document.getElementById(`agregar${producto.id}`)
+
+
+    boton.addEventListener('click', () => {
+        agregarAlCarrito(producto.id)
+ 
+    })
+})
+
+const agregarAlCarrito = (prodId)=>{
+    const item = stockProductos.find ((prod) => prod.id ===prodId)
+    carrito.push(item)
+    actualizarCarrito()
+    console.log(carrito)
+}
+
+
+
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId)
+    const indice = carrito.indexOf(item) 
+    carrito.splice(indice, 1) 
+    actualizarCarrito() 
+    console.log(carrito)
+}
+
+
+const actualizarCarrito = () => {
+    contenedorCarrito.innerHTML = "" 
+    carrito.forEach((prod) => {
+        const div = document.createElement('div')
+        div.className = ('productoEnCarrito')
+        div.innerHTML = `
+        <p>${prod.nombre}</p>
+        <p>Precio:$${prod.precio}</p>
+        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+        `
+
+        contenedorCarrito.appendChild(div)
+        
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+
+    })
+    contadorCarrito.innerText = carrito.length
+    precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.precio, 0)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let carrito = []
+let eleccion = prompt("Quisiera comprar algun producto?")
 while (eleccion !="si" && eleccion !="no"){
     alert("por favor responda por si o por no")
     eleccion = prompt("Quisiera comprar algun producto?")
 }
-if (eleccion = "si"){
+if (eleccion == "si"){
     alert("Esta es nuestra lista de productos")
     let catalogo = productos.map(
         (producto) => producto.nombre + " " + "$" + producto.precio
@@ -91,4 +216,5 @@ while(eleccion != "no"){
 }
 
 const total = carrito.reduce ((acc, el) => acc + el.precio * el.unidades, 0)
-console.log(`el total a pagar por su compra es: $ ${total}`)
+console.log(`el total a pagar por su compra es: $ ${total}`)*/
+
